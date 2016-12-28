1c47e57f7accf510d406a261d1f74a4117172d59
Vladimir Pavković <vladimirpavk@gmail.com>
2016-12-28 14:55:35 +0100

    Auth0

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,17 @@
 /// <reference path="../../node_modules/@angular/common/index.d.ts" />
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
 @Component({
   selector: 'my-app',
-  templateUrl: './app/ts/app.component.html'
+  //templateUrl: './app/ts/app.component.html',
+  template: `<button (click)="auth.login()">Login</button>"`,
+  providers: [ AuthService ]
 })
 export class AppComponent{
+
+  constructor(private auth: AuthService){
+
+  }
+
+
 }
